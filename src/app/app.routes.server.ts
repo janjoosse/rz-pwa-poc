ba962234dcd18acdae2f3d5958c9055f5a34c294
janjoosse <24371035+janjoosse@users.noreply.github.com>
2026-03-10 23:42:56 +0100

    Add support for prerendered routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,6 +8,10 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'page/:slug',
     renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const routesJson = await fetch('../../routes.json').then(res => res.json());
+      return routesJson.routes.map((route: { slug: string }) => ({ slug: route.slug }));
+    }
   },
   {
     path: '**',
